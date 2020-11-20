ec86da1d151c2288610f78f1a84b25ffd4705e9b
Ankitkj1999 <57802410+Ankitkj1999@users.noreply.github.com>
2020-11-20 21:34:42 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/gfg_boxshadow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gfg_boxshadow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    //Our app widget tree starts herwe
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('GeeksforGeeks'),
+          backgroundColor: Colors.greenAccent[400],
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Menu',
+            onPressed: () {},
+          ), //IcoButton
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.comment),
+              tooltip: 'Comment',
+              onPressed: () {},
+            ), //IconButton
+          ], //<Widget>[]
+        ), //AppBar
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: SizedBox(
+              height: 200,
+              width: 250,
+              child: Container(
+                  decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: NetworkImage(
+                      'https://media.geeksforgeeks.org/wp-content/cdn-uploads/logo.png'),
+                  scale: 3.0,
+                ),
+                border: Border.all(
+                    color: Colors.green,
+                    width: 4.0,
+                    style: BorderStyle.solid), //Border.all
+                /* The BorderRadius widget  is here */
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0),
+                  bottomLeft: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0),
+                ), //BorderRadius.only
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.greenAccent[200],
+                    offset: const Offset(
+                      5.0,
+                      5.0,
+                    ),
+                    blurRadius: 10.0,
+                    spreadRadius: 2.0,
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: const Offset(0.0, 0.0),
+                    blurRadius: 5.0,
+                    spreadRadius: 1.0,
+                  ),
+                ],
+                //BoxDecoration
+              )),
+            ),
+          ),
+        ), //Center
+      ), //Scaffold
+      debugShowCheckedModeBanner: false, //Deug banner is turned off
+    ), //MaterialApp
+  );
+}
